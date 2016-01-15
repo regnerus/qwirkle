ac8386089af7bea40858bc41b26638cd66e5427c
ChrisTerBeke <ctbeke@gmail.com>
2016-01-15 01:07:42 +0100

    start folder structure

---
+++ Qwirkle.java
@@ -0,0 +1,13 @@
+package qwirkle;
+
+/**
+ * The Qwirkle Main
+ *
+ * Created by Chris ter Beke and Bouke Regnerus
+ */
+public class Qwirkle {
+
+    public static void main(String[] args) {
+        // TODO: implement gameplay
+    }
+}
---
+++ src/qwirkle/server/Server.java
@@ -0,0 +1,15 @@
+package qwirkle.server;
+
+// needed packages for networking
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * The Qwirkle Server
+ */
+public class Server {
+
+}
