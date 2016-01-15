b766543680e720b48de609462694521089d7c59d
ChrisTerBeke <ctbeke@gmail.com>
2016-01-15 01:56:28 +0100

    fix issues

--- src/qwirkle/server/QwirkleServer.java
+++ src/qwirkle/server/QwirkleServer.java
@@ -1,14 +1,9 @@
 package qwirkle.server;
 
-// needed packages for networking
+// java
 import java.io.IOException;
-import java.net.UnknownHostException;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-// shared
-import qwirkle.shared.Protocol;
 
 // controllers
 import qwirkle.server.controllers.ClientController;
@@ -16,31 +11,30 @@ import qwirkle.server.controllers.ClientController;
 /**
  * The Qwirkle Server
  */
-public class Server {
+public class QwirkleServer {
 
     // port to run server on
     private int port;
 
-    public Server(int _port) {
+    public QwirkleServer(int _port) {
         port = _port;
-
-
     }
 
     /**
      * Starts the game server on selected port
      */
     public void start() {
+
         // use try catch around setting up server, port may be in use!
         try {
             ServerSocket serverSock = new ServerSocket(port);
 
             while(true) {
                 // start accepting socket connections
-                Socket sock = serverSock.accept();
+                Socket socket = serverSock.accept();
 
                 // TODO: populate ClientController
-                ClientController client = new ClientController();
+                ClientController client = new ClientController(this, socket, 0);
 
                 // client.start();
             }
@@ -48,19 +42,6 @@ public class Server {
         catch (IOException e) {
             // TODO: use CLI output
             System.out.println(e);
-        }
-    }
-
-    public void handleClientInput(String msg, ClientController client) {
-
-        // the protocol states we should split the incoming string
-        String input = msg;
-        String[] split = msg.split(" ");
-
-        switch (split[0]) {
-            case Protocol.HALLO:
-                // do something with HALLO
-                break;
         }
     }
 }
--- src/qwirkle/server/controllers/ClientController.java
+++ src/qwirkle/server/controllers/ClientController.java
@@ -1,9 +1,130 @@
 package qwirkle.server.controllers;
 
+// java
+import java.io.IOException;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// shared
+import qwirkle.shared.Protocol;
+
+// server
+import qwirkle.server.QwirkleServer;
+
 /**
  * ClientController
  * Handles clients that are connecting to the game server
  */
-public class ClientController {
+public class ClientController extends Thread {
 
+    private BufferedReader in;
+    private BufferedWriter out;
+
+    private int playerId;
+
+
+    public ClientController(QwirkleServer server, Socket _socket, int _playerId) throws IOException {
+        this.playerId = _playerId;
+
+        in = new BufferedReader(new InputStreamReader(_socket.getInputStream()));
+        out = new BufferedWriter(new OutputStreamWriter(_socket.getOutputStream()));
+    }
+
+    /**
+     * Do this when successfully connected to server
+     */
+    public void handleConnect() {
+
+    }
+
+    public void handleMessage(String msg, ClientController client) {
+
+        // the protocol states we should split the incoming string
+        String[] input = msg.split(" ");
+
+        switch (input[0]) {
+
+            case Protocol.Client.HALLO:
+                // do something with HALLO
+                break;
+
+            case Protocol.Client.QUIT:
+
+                break;
+
+            case Protocol.Client.INVITE:
+
+                break;
+
+            case Protocol.Client.ACCEPTINVITE:
+
+                break;
+
+            case Protocol.Client.DECLINEINVITE:
+
+                break;
+
+            case Protocol.Client.MAKEMOVE:
+
+                break;
+
+            case Protocol.Client.CHAT:
+
+                break;
+
+            case Protocol.Client.REQUESTGAME:
+
+                break;
+
+            case Protocol.Client.CHANGESTONE:
+
+                break;
+
+            case Protocol.Client.GETLEADERBOARD:
+
+                break;
+
+            case Protocol.Client.GETSTONESINBAG:
+
+                break;
+
+            case Protocol.Client.ERROR:
+                // client is failing hard
+                break;
+        }
+    }
+
+    /**
+     * Get the playerId of the client
+     * @return int playerId
+     */
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    /**
+     * Handles a disconnect of the client
+     */
+    public void handleDisconnect() {
+
+    }
+
+    /**
+     * Send a message to the client
+     */
+    public void emit(String data) {
+        // try catch around sending a message over socket
+        try {
+            out.write(data);
+            out.newLine();
+            out.flush();
+        }
+        catch (IOException e) {
+            // TODO: handle error in CLI
+            e.printStackTrace();
+        }
+    }
 }
