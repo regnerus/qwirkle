a2db53804b55dcd1a77570d23bf2c96c190bd0bf
Bouke Regnerus <bouke@regner.us>
2016-01-22 20:16:20 +0100

    Validate Adjacent Point

--- src/qwirkle/game/Board.java
+++ src/qwirkle/game/Board.java
@@ -16,8 +16,26 @@ public class Board {
         this.board = new HashMap<>();
     }
 
+    public Map<Point2D, Stone> getBoard () {
+        return this.board;
+    }
+
     public void placeStone(Stone stone) {
+        //TODO: Validate stone position with validateAdjacentPoint
+
         board.put(stone.getPoint(), stone);
+    }
+
+    public Stone getStone(Point2D point) {
+        return board.get(point);
+    }
+
+    private boolean validateAdjacentPoint(Stone currentStone, int directionX, int directionY) {
+        Point2D currentPosition = currentStone.getPoint();
+        Point2D adjacentPoint = new Point2D.Double(currentPosition.getX() - directionX, currentPosition.getY() - directionY);
+        Stone adjacentStone = this.getStone(adjacentPoint);
+
+        //TODO: Add validation for allowed situations.
     }
 
 
