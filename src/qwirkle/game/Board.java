bc59d50c5d16278bddfb8caf2a56734d120e9fdb
Bouke Regnerus <bouke@regner.us>
2016-01-22 20:00:03 +0100

    Started Board and Stone Positions

--- src/qwirkle/game/Board.java
+++ src/qwirkle/game/Board.java
@@ -1,7 +1,27 @@
 package qwirkle.game;
 
+import java.awt.geom.Point2D;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by chris on 15/01/16.
  */
 public class Board {
+
+
+    Map<Point2D, Stone> board;
+
+    public Board () {
+        this.board = new HashMap<>();
+    }
+
+    public void placeStone(Stone stone) {
+        board.put(stone.getPoint(), stone);
+    }
+
+
+
 }
+
+
--- src/qwirkle/game/Stone.java
+++ src/qwirkle/game/Stone.java
@@ -1,7 +1,18 @@
 package qwirkle.game;
 
+import java.awt.geom.Point2D;
+
 /**
  * Created by chris on 15/01/16.
  */
 public class Stone {
+    Point2D point;
+
+    public void setPoint (int x, int y) {
+        this.point = new Point2D.Double(x, y);
+    }
+
+    public Point2D getPoint () {
+        return point;
+    }
 }
