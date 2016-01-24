271bad231181d0c96a044e009609ec838ba88d74
Bouke Regnerus <bouke@regner.us>
2016-01-24 02:30:02 +0100

    Removed some comments.

--- src/qwirkle/game/Move.java
+++ src/qwirkle/game/Move.java
@@ -45,49 +45,4 @@ public class Move {
         }
         return true;
     }
-
-//    public boolean validateConnectedRow(List<Stone> row) {
-//        Set<Stone.Shape> shapes = new HashSet<>();
-//        Set<Stone.Color> colors = new HashSet<>();
-//        Stone previousStone = null;
-//
-//        boolean isUniqueShape = true;
-//        boolean isUniqueColor = true;
-//        boolean isSameShape = true;
-//        boolean isSameColor = true;
-//
-//        for (Stone stone : row) {
-//            //Checks if the shape of all stones in the row is unique
-//            if(!shapes.contains(stone.getShape())) {
-//                shapes.add(stone.getShape());
-//            }
-//            else {
-//                isUniqueShape = false;
-//            }
-//
-//            //Checks if the color of all stones in the row is unique
-//            if(!colors.contains(stone.getColor())) {
-//                colors.add(stone.getColor());
-//            }
-//            else {
-//                isUniqueColor = false;
-//            }
-//
-//            //Checks if the shape of all stones in the row is the same
-//            if(previousStone != null && previousStone.getShape() != stone.getShape()) {
-//                isSameShape = false;
-//            }
-//
-//            //Checks if the color of all stones in the row is the same
-//            if(previousStone != null && previousStone.getShape() != stone.getShape()) {
-//                isSameShape = false;
-//            }
-//
-//            previousStone = stone;
-//        }
-//
-//        //Returns true if the row has a combination of unique shapes with the same color OR a unique color with the same shapes.
-//        return (isUniqueShape && isSameColor) || (isUniqueColor && isSameShape);
-//
-//    }
 }
